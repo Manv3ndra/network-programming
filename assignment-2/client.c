a07b054d75ad1e2d21005f5ec4bd1f41a1c6d132
Rohit Kumar <rkumar_be20@thapar.edu>
2022-09-06 13:17:35 +0530

    updated

--- assignment-2/client.c
+++ assignment-2/client.c
@@ -1,33 +1,51 @@
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#define MAXLINE 1024
 
 int main()
 {
     int sock_fd;
     struct sockaddr_in servaddr;
+    char buffer[MAXLINE];
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd == -1)
+    {
+        printf("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
     bzero(&servaddr, sizeof servaddr);
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(22000);
-    connect(sock_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (connect(sock_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("Connection failed");
+        exit(EXIT_FAILURE);
+    }
     while (1)
     {
-        char sendline[100];
-        char recvline[100];
-        bzero(sendline, 100);
-        bzero(recvline, 100);
-        fgets(sendline, 100, stdin);
-        send(sock_fd, sendline, strlen(sendline), 0);
-        if (strcasecmp(sendline, "quit\n") == 0)
+        bzero(buffer, MAXLINE);
+        fgets(buffer, MAXLINE, stdin);
+        if (send(sock_fd, buffer, strlen(MAXLINE), 0) < 0)
+        {
+            printf("Send failed");
+            exit(EXIT_FAILURE);
+        }
+        if (strcasecmp(buffer, "quit\n") == 0)
         {
             break;
         }
-        recv(sock_fd, recvline, 100, 0);
-        printf("Message from Server: %s", recvline);
+        bzero(buffer, MAXLINE);
+        if (recv(sock_fd, buffer, MAXLINE, 0) < 0)
+        {
+            printf("Receive failed");
+            exit(EXIT_FAILURE);
+        }
+        printf("Message from Server: %s", buffer);
     }
     close(sock_fd);
 }
--- assignment-2/server.c
+++ assignment-2/server.c
@@ -1,32 +1,56 @@
-#include <sys/socket.h>
-#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#define MAXLINE 1024
 
 int main()
 {
     int listen_fd;
     struct sockaddr_in servaddr;
+    char buffer[MAXLINE];
     listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1)
+    {
+        printf("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(22000);
-    bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-    listen(listen_fd, 10);
+    if (bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(listen_fd, 10) < 0)
+    {
+        printf("Listen failed");
+        exit(EXIT_FAILURE);
+    }
     int comm_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL);
     while (1)
     {
-        char str[100];
-        bzero(str, 100);
-        recv(comm_fd, str, 100, 0);
-        if (strcasecmp(str, "exit\n") == 0)
+        bzero(buffer, MAXLINE);
+        if (recv(comm_fd, buffer, MAXLINE, 0) < 0)
+        {
+            printf("Receive failed");
+            exit(EXIT_FAILURE);
+        }
+        printf("Message from Client: %s", buffer);
+        if (strcasecmp(buffer, "exit\n") == 0)
         {
             break;
         }
-        printf("Echoing back - %s", str);
-        send(comm_fd, str, strlen(str), 0);
+        printf("Echoing back : %s", buffer);
+        if (send(comm_fd, buffer, MAXLINE, 0) < 0)
+        {
+            printf("Send failed");
+            exit(EXIT_FAILURE);
+        }
     }
     close(comm_fd);
 }
